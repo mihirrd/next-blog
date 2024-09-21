efda310f559c43b411faea0c6db49bc9999e10fe
mihird <mihir.deshpande@helpshift.com>
2024-09-21 16:33:03 +0530

    fix typos in about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,9 +11,9 @@ const page = () => {
         <NavBar/>
         <section className="flex justify-center mb-1 font-serif">
             <div className="flex flex-col p-10 text-justify w-full lg:w-2/5 md:text-lg">
-            <p className='mb-10 mt-8'>
+            <p className='mb-10'>
               Hey there, my name is Mihir Deshpande. 
-              I am Software Engineer. I am currently working in Helpshift as a Backend Engineer. Now as days, I am mostly "functioning" in Clojure. I specialise in Product Development and Distributed Systems. I mostly write here about literally anything that I find fascinating. 
+              I am a Software Engineer. I am currently working in Helpshift as a Backend Engineer. I am proficient in Java and Python. Now a days, I am mostly "functioning" in Clojure. I specialise in Product Development and Distributed Systems.
               </p>
               <div className="flex justify-center mb-10">
               <Image
@@ -25,7 +25,7 @@ const page = () => {
               ></Image>
               </div>
               <p className='mb-10' >
-              I am also a Sitar Player and a student of Indian Classial Music. You can find my music on Youtube and spotify and some reels on Instagram as well!
+              I am also a Sitar Player and a student of Indian Classial Music. You can find my music on Youtube, spotify and on Instagram reels!
               </p>
               <div className="flex justify-center mb-10">
               <Image
