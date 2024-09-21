cdb7e6387b1869248fd34a274fa788cac15cec12
mihird <mihir.deshpande@helpshift.com>
2024-09-21 16:16:18 +0530

    Fix about layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
     <main>
         <NavBar/>
         <section className="flex justify-center mb-1 font-serif">
-            <div className="flex flex-col p-10 text-justify w-4/5 lg:w-2/5 md:text-lg">
+            <div className="flex flex-col p-10 text-justify w-full lg:w-2/5 md:text-lg">
             <p className='mb-10 mt-8'>
               Hey there, my name is Mihir Deshpande. 
               I am Software Engineer. I am currently working in Helpshift as a Backend Engineer. Now as days, I am mostly "functioning" in Clojure. I specialise in Product Development and Distributed Systems. I mostly write here about literally anything that I find fascinating. 
