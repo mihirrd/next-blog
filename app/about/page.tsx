08e4880d79e2af3e4dcfcc244976f6f8ae6bec71
Mihir Deshpande <deshpande.mihir7@gmail.com>
2025-02-14 13:13:02 +0530

    Edit about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
       <section className="flex justify-center mb-1 font-serif text-justify">
         <div className="flex flex-col p-10 w-full lg:w-2/5 md:text-lg">
           <p className='mb-10 prose md:prose-xl'>
-            Hey there, my name is Mihir Deshpande. I am a Programmer and a Software Engineer. I specialize in Product Development and Distributed Systems.
+            Hey there, my name is Mihir Deshpande. I am a Programmer and a Software Engineer. I work on Product Development and Distributed Systems.
             <br />
             I’m passionate about exploring tech, <a href='https://www.paulgraham.com/hp.html' target="blank" className=''>hacking</a> together cool projects and building robust and efficient software. From functional programming and distributed systems to containerization and collaborative development, I’ve built a well-rounded skill set. I have a solid foundation in functional programming, distributed systems, and modern development tools. I enjoy tackling complex systems, refining workflows, and creating impactful and well-documented solutions.
           </p>
