db4ae0ec81623ef85e89a783c5ce842ad7ef2c49
Mihir Deshpande <deshpande.mihir7@gmail.com>
2025-04-28 23:36:57 +0530

    Move about me to markdown

---
+++ app/about/about.md
@@ -0,0 +1,58 @@
+---
+title: 'About me'
+---
+
+Hey there, my name is Mihir. I am a software engineer with 4 years of experience building scalable, high-performance backend systems. I am passionate about designing efficient and fault-tolerant system architectures. I am skilled in integrating LLMs into production environments, with hands-on experience in prompt engineering and GenAI implementation. I enjoy tackling complex problems, drive high-impact solutions, and lead projects that enhance system reliability, performance, and scalability. I love exploring tech and [hacking](https://www.paulgraham.com/hp.html) together cool projects. I love Emacs!
+
+--- 
+
+### Work Experience
+At Helpshift, I’ve led initiatives that include:
+- Designing a high-throughput architecture for LLM integration, supporting 1.5K RPS
+- Engineering a Kafka-based asynchronous task queue that cut load average by 30%
+- Building multilingual AI-powered translation systems that increased user engagement by 20%
+- Mentoring new engineers, driving clean code practices and team-wide efficiency
+
+---
+
+### Projects and Open Source Contribution
+My open-source work in graph embeddings (clj-deepwalk) has been featured in Clojure Deref, and I continue to experiment with tools at the intersection of AI and systems.
+
+#### [Clj-deepwalk](https://github.com/mihirrd/clj-deepwalk)
+- Graph Embedding library written Clojure.
+- Implements [Deepwalk](https://arxiv.org/abs/1403.6652) for learning graph node embeddings
+- Enables vector-based reasoning and similarity search over graph structures
+- Lightweight, modular, and easy to extend for research or production use
+
+
+#### [Ollama MCP Client](https://github.com/mihirrd/ollama-mcp-client)
+- Seamless integration between Ollama language models and MCP servers
+- Native support for Git operations via MCP Git server
+- Extensible architecture for managing multiple MCP servers
+- Rich, interactive Terminal UI for a smooth user experience
+
+#### [AIde](https://github.com/mihirrd/aide)
+- Generates coding challenges using OpenAI
+- Evaluates user-submitted solutions automatically
+- Provides detailed, actionable feedback
+- Helps developers sharpen coding skills through interactive practice
+
+--- 
+
+🎓 I’ll be joining Purdue, West Lafayette in Fall 2025 for MS in Software Engineering, with a focus on distributed systems and backend architecture.
+
+Let’s connect if you're hiring for backend or platform roles — or if you're working on systems where performance, reliability, and scalability are top priorities.
+
+📧 Email : deshpande.mihir7@gmail.com | deshpa92@purdue.edu
+
+<br>
+<p align="center">
+<img src="/images/headshot.png" style="border-radius: 12px;" alt="" width="500"/>
+</p>
+<br>
+
+On a sidenote, I am also a Sitar Player and a student of Indian Classical Music. You can find my music on Youtube, Spotify and on Instagram!
+
+<p align="center">
+<img src="/images/concert.png" style="border-radius: 12px;" alt="" width="600"/>
+</p>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,49 +1,34 @@
 import React from 'react'
-import Image from 'next/image'
-import profilePic from '../../public/images/headshot.png'
-import concertPic from '../../public/images/concert.png'
+import Markdown from "markdown-to-jsx"
+import fs from 'fs'
+import matter from "gray-matter"
+import path from 'path'
+import Divider from '@/app/components/Divider'
+import getPostMetadata from '@/app/utils/getPostMetadata'
 import Link from 'next/link'
+import { Metadata } from 'next'
+
+function getAboutContent(slug) {
+    const folder = path.join(process.cwd(), '/app/about/')
+    const file = folder + 'about.md'
+    const content = fs.readFileSync(file, 'utf8')
+    const matterResult = matter(content)
+    return {
+        title: matterResult.data.title,
+        content: matterResult.content,
+    }
+}
 
 const page = () => {
+  const { title, content } = getAboutContent('about')
+
   return (
     <main>
       <section className="flex justify-center font-serif text-justify">
         <div className="flex flex-col p-10 w-full lg:w-2/5 md:text-lg">
-          <p className='mb-4 prose md:prose-xl'>
-            Hey there, my name is Mihir. I am a software engineer with 4 years of experience building scalable, high-performance backend systems. I am passionate about designing efficient and fault-tolerant system architectures. I am skilled in integrating LLMs into production environments, with hands-on experience in prompt engineering and GenAI implementation. I enjoy tackling complex problems, drive high-impact solutions, and lead projects that enhance system reliability, performance, and scalability.
-            I love exploring tech and <a href='https://www.paulgraham.com/hp.html' target="blank" className=''>hacking</a> together cool projects.
-            <br/>
-            <br/>
-            This is the place where I post my thoughts and learnings.
-          </p>
-          <div className='text-center mb-4'>
-            <Link href='Resume_Mihir_Deshpande.pdf'
-              target='_blank'
-              rel="noopener noreferrer">
-              <p className='underline'>View Resume</p>
-            </Link>
-          </div>
-          <div className="flex justify-center mb-10">
-            <Image
-              className="rounded-badge"
-              src={profilePic}
-              alt=''
-              height={600}
-              width={600}
-            ></Image>
-          </div>
-          <p className='mb-10 prose md:prose-xl'>
-            On a sidenote, I am also a Sitar Player and a student of Indian Classical Music. You can find my music on Youtube, Spotify and on Instagram!
-          </p>
-          <div className="flex justify-center mb-10">
-            <Image
-              className="object-cover rounded-xl"
-              src={concertPic}
-              alt=''
-              height={600}
-              width={600}
-            ></Image>
-          </div>
+        <article className='prose prose-h4:font-thin prose-h3:font-thin prose-code:font-thin prose-code:text-sm md:prose-xl text-justify'>
+        <Markdown>{content}</Markdown>
+        </article>
         </div>
       </section>
     </main>
